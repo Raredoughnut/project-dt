555be70eb7224606ee195cc0827c1d0e06bbaf54
baeksangsu-agricore <baeksangsu@it.farmfam.co.kr>
2026-07-10 13:59:48 +0900

    ads.txt 및 메타태그 추가

--- src/components/google-adsense.tsx
+++ src/components/google-adsense.tsx
@@ -6,10 +6,14 @@ export function GoogleAdSense() {
   // 애드센스 콘솔이 제공하는 스니펫과 동일하게 <head>에 서버 렌더링되도록
   // next/script(afterInteractive: 하이드레이션 후 클라이언트 주입) 대신 순수 <script> 사용
   return (
-    <script
-      async
-      crossOrigin="anonymous"
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADSENSE_CLIENT_ID}`}
-    />
+    <>
+      {/* 구글이 스크립트 태그를 못 잡을 때를 대비한 소유권 검증용 메타 태그 */}
+      <meta name="google-adsense-account" content={GOOGLE_ADSENSE_CLIENT_ID} />
+      <script
+        async
+        crossOrigin="anonymous"
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADSENSE_CLIENT_ID}`}
+      />
+    </>
   );
 }
